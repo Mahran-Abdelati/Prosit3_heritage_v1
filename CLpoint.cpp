3cc96dc8771abcfd33f7bf38542c87d2d17d0ad8
Mahran-Abdelati <mahran.abdelati@viacesi.fr>
2023-11-17 15:17:41 +0100

    Création de la classe CLpoint

--- CLpoint.cpp
+++ CLpoint.cpp
@@ -1,1 +1,49 @@
 #include "CLpoint.h"
+#include <iostream>
+
+using namespace std;
+
+CLpoint::CLpoint(void)
+{
+	this -> x = 0;
+	this -> y = 0;
+}
+
+CLpoint::CLpoint(int x, int y)
+{
+	this -> x = x;
+	this -> y = y;
+}
+
+void CLpoint::set_point(int x, int y)
+{
+	this -> x = x;
+	this -> y = y;
+}
+
+int CLpoint::get_x()
+{
+	return this -> x;
+}
+
+int CLpoint::get_y()
+{
+	return this -> y;
+}
+
+int CLpoint::get_z()
+{
+	return this -> z;
+}
+
+void CLpoint::deplacer_point(int x, int y)
+{
+	this -> x = this -> x + x;
+	this -> y = this -> y + y;
+}
+
+void CLpoint::afficherCoordo()
+{
+	cout << "Composante x du point : " << this -> x << endl;
+	cout << "Composante y du point : " << this -> y << endl;
+}
--- CLpoint.h
+++ CLpoint.h
@@ -1,5 +1,19 @@
 #pragma once
+
 class CLpoint
 {
+private :
+	int x;
+	int y;
+	int z;
+public :
+	CLpoint(void);
+	CLpoint(int x, int y);
+	virtual void set_point(int x, int y);
+	virtual int get_x();
+	virtual int get_y();
+	virtual int get_z();
+	virtual void deplacer_point(int x, int y);
+	virtual void afficherCoordo();
 };
 
